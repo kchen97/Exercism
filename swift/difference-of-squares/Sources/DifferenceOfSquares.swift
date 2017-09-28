9331128370972983718a7f17aee66619bbc7bfe4
Korman Chen <kormanchen@Kormans-MacBook-Pro.local>
2017-09-28 10:46:47 -0700

    Added Difference of Squares

---
+++ swift/difference-of-squares/Sources/DifferenceOfSquares.swift
@@ -0,0 +1,23 @@
+//Solution goes in Sources
+
+class Squares
+{
+    var squareOfSums: Int
+    var sumOfSquares: Int
+    var differenceOfSquares: Int
+    
+    init(_ n: Int)
+    {
+        self.squareOfSums = 0
+        self.sumOfSquares = 0
+        
+        for i in 0...n { //Runtime 2t * N = O(N)
+            squareOfSums += i //t
+            sumOfSquares += (i * i) //t
+        }
+        
+        self.squareOfSums *= squareOfSums
+        
+        self.differenceOfSquares = abs(squareOfSums - sumOfSquares)
+    }
+}
